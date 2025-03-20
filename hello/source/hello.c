cf3f0ee2330c329c999a14f529d0fe98b7c8be4e
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-03-20 15:05:46 +0900

    点のサイズを変更

--- hello/source/hello.c
+++ hello/source/hello.c
@@ -1,9 +1,13 @@
 
 int main()
 {
-  *(unsigned int*)0x04000000=0x0403;
-  ((unsigned short*)0x06000000)[120+80*240]=0x001f;
-  ((unsigned short*)0x06000000)[136+80*240]=0x03e0;
-  ((unsigned short*)0x06000000)[120+96*240]=0x7c00;
-  while(1);
+    *(unsigned int*)0x04000000=0x0403;
+    for ( int y = 0; y < 4; ++ y ) {
+        for ( int x = 0; x < 8; ++ x ) {
+            ((unsigned short*)0x06000000)[(120+y)+(80+x)*240]=0x001f;
+            ((unsigned short*)0x06000000)[(136+y)+(80+x)*240]=0x03e0;
+            ((unsigned short*)0x06000000)[(120+y)+(96+x)*240]=0x7c00;
+        }
+    }
+    while(1);
 }
