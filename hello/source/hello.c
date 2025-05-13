261e13e8816a953b441b78e2e3298d0a96477acb
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-05-13 23:31:09 +0900

    動作確認用に点のサイズを変更

--- hello/source/hello.c
+++ hello/source/hello.c
@@ -4,8 +4,8 @@ int main()
     *(unsigned int*)0x04000000=0x0403;
 loop:
     for ( int i = 0; i < 0x7fff; i += 0x3fff ) {
-        for ( int y = 0; y < 4; ++ y ) {
-            for ( int x = 0; x < 8; ++ x ) {
+        for ( int y = 0; y < 2; ++ y ) {
+            for ( int x = 0; x < 3; ++ x ) {
                 ((unsigned short*)0x06000000)[(120+x)+(80+y)*240]=0x001f;
                 ((unsigned short*)0x06000000)[(136+x)+(80+y)*240]=0x03e0;
                 ((unsigned short*)0x06000000)[(120+x)+(96+y)*240]=0x7c00;
