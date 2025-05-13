7a7fc941da04105e9378cd6491ef31d2d36f69b6
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-05-13 12:45:05 +0900

    点を追加

--- hello/source/hello.c
+++ hello/source/hello.c
@@ -2,12 +2,17 @@
 int main()
 {
     *(unsigned int*)0x04000000=0x0403;
-    for ( int y = 0; y < 4; ++ y ) {
-        for ( int x = 0; x < 8; ++ x ) {
-            ((unsigned short*)0x06000000)[(120+x)+(80+y)*240]=0x001f;
-            ((unsigned short*)0x06000000)[(136+x)+(80+y)*240]=0x03e0;
-            ((unsigned short*)0x06000000)[(120+x)+(96+y)*240]=0x7c00;
+loop:
+    for ( int i = 0; i < 0x7fff; i ++ ) {
+        for ( int y = 0; y < 4; ++ y ) {
+            for ( int x = 0; x < 8; ++ x ) {
+                ((unsigned short*)0x06000000)[(120+x)+(80+y)*240]=0x001f;
+                ((unsigned short*)0x06000000)[(136+x)+(80+y)*240]=0x03e0;
+                ((unsigned short*)0x06000000)[(120+x)+(96+y)*240]=0x7c00;
+                ((unsigned short*)0x06000000)[(136+x)+(96+y)*240]=i;
+            }
         }
     }
+    goto loop;
     while(1);
 }
