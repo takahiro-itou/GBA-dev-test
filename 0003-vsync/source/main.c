48dbb3bebf7317a40ca3c8eb5ae39619f55a745c
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-05-19 12:41:59 +0900

    垂直帰線期間を待つ処理を実装

--- 0003-vsync/source/main.c
+++ 0003-vsync/source/main.c
@@ -1,6 +1,15 @@
 
 #include    <gba.h>
 
+void waitVsync()
+{
+    while ( REG_VCOUNT >= 160 ) { }
+    while ( REG_VCOUNT <  160 ) { }
+}
+
 int main()
 {
+    for (;;) {
+        waitVsync();
+    }
 }
