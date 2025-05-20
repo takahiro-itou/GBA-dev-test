ee3a6937d8b439f29b063bef5bf9b4833758fe13
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-05-20 12:41:07 +0900

    フレームの切り替えを実装

--- 0005-bg4-1/source/main.c
+++ 0005-bg4-1/source/main.c
@@ -1,6 +1,17 @@
 
 #include    <gba.h>
 
+#define     VRAM_FRAME_0    (u8 *)(0x06000000)
+#define     VRAM_FRAME_1    (u8 *)(0x0600A000)
+
+u8 *    screen;
+
+void toggleFrame()
+{
+    REG_DISPCNT ^= BACKBUFFER;
+    screen  = (REG_DISPCNT & BACKBUFFER) ? VRAM_FRAME_0 : VRAM_FRAME_1;
+}
+
 int main()
 {
     for (;;) { }
